f67a9f6d1c7a6e48fb1a8292a90a096ca4532b28
wangyong22 <wangyong2@gmail.com>
2024-03-29 21:55:48 +0800

    change 3

--- BJDemoApp1/ContentView.swift
+++ BJDemoApp1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, SE 2 world!")
+            Text("Hello, SE 3 world!")
         }
         .padding()
     }
