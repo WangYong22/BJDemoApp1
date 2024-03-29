53910994e9878663c437d2a958357b46a1caabe7
wangyong22 <wangyong2@gmail.com>
2024-03-29 20:36:28 +0800

    Initial Commit

---
+++ BJDemoApp1/BJDemoApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  BJDemoApp1App.swift
+//  BJDemoApp1
+//
+//  Created by mdsadmin on 2024/3/29.
+//
+
+import SwiftUI
+
+@main
+struct BJDemoApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
