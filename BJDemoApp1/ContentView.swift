d69521f103fac23c75d8992ab1e5f0c518c6eaa7
wangyong22 <wangyong2@gmail.com>
2024-03-30 23:11:20 +0800

    add 8

--- BJDemoApp1/ContentView.swift
+++ BJDemoApp1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, SE 7 world!")
+            Text("Hello, SE 8 world!")
         }
         .padding()
     }
