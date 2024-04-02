b864ef79ea572d703b6ab6d55cc3810ca995b2ed
wangyong22 <wangyong2@gmail.com>
2024-04-02 11:07:01 +0800

    13

--- BJDemoApp1/ContentView.swift
+++ BJDemoApp1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, SE 12 world!")
+            Text("Hello, SE 13 world!")
         }
         .padding()
     }
